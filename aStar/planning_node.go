024a0a44834e468d692e66b55b19c7ec87b6d039
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2024-03-02 09:00:24 -0500

    Added Functions for Calculating Costs and Updating them based on the current heuristic

--- aStar/planning_node.go
+++ aStar/planning_node.go
@@ -22,9 +22,11 @@ Description:
 	An implementation of a PlanningNode for use in the A* algorithm.
 */
 type PlanningNode struct {
-	Graph            graph.WeightedUndirected //TODO: Change to graph.WeightedUndirected (a more general type)
+	Graph            graph.WeightedUndirected
 	CurrentGraphNode graph.Node
 	PreviousInPlan   *PlanningNode
+	CostToGo         float64 // The cost to go from the current node to the goal node
+	HeuristicCost    float64 // The heuristic cost from the current node to the goal node
 }
 
 // =======
@@ -35,9 +37,39 @@ type PlanningNode struct {
 Cost
 Description:
 
-	Returns the cost of the current plan based on the .
+	Returns the cost of the current planning node based on the
+	cost of the previous node and the edge between the previous
 */
 func (pn *PlanningNode) Cost() float64 {
+	return pn.HeuristicCost
+}
+
+/*
+CalculateCosts
+Description:
+
+	Calculates the cost to go and the heuristic cost for the current node.
+*/
+func (pn *PlanningNode) UpdateCosts(
+	heuristic func(int64, graph.WeightedUndirected) float64,
+) {
+	// Setup
+
+	// Algorithm
+	pn.CostToGo = pn.CalculateCostToGo()
+	pn.HeuristicCost = pn.CalculateHeuristicCost(heuristic)
+}
+
+/*
+CalculateCostToGo
+Description:
+
+	Calculates the cost to go to the current node using:
+	- the cost of the previous node,
+	- the edge between the previous node and the current node, as well as
+	- the heuristic cost from the current node to the goal node.
+*/
+func (pn *PlanningNode) CalculateCostToGo() float64 {
 	//Constants
 	underlyingGraph := pn.Graph
 
@@ -55,5 +87,59 @@ func (pn *PlanningNode) Cost() float64 {
 		pn.CurrentGraphNode.ID(),
 	)
 	return pn.PreviousInPlan.Cost() + lastEdge.Weight()
+
+}
+
+/*
+CalculateHeuristicCost
+Description:
+
+	Calculates the cost to go to the current node using:
+	- the cost of the previous node,
+	- the edge between the previous node and the current node, as well as
+	- the heuristic cost from the current node to the goal node.
+
+Notes:
+
+  - This function assumes that the CostToGo has already been defined.
+*/
+func (pn *PlanningNode) CalculateHeuristicCost(heuristic func(int64, graph.WeightedUndirected) float64) float64 {
+
+	return heuristic(pn.CurrentGraphNode.ID(), pn.Graph) + pn.CostToGo
+}
+
+/*
+Expand
+Description:
+
+	"Expands" from the current graph node to all of the adjacent nodes.
+	Returns a slice of PlanningNodes that represent the expanded nodes.
+*/
+func (pn *PlanningNode) Expand(heuristic func(int64, graph.WeightedUndirected) float64) []*PlanningNode {
+	// Setup
+
+	// Create a slice to hold the expanded nodes
+	var expandedNodes []*PlanningNode
+	neighboringNodes := pn.Graph.From(pn.CurrentGraphNode.ID())
+	for neighboringNodes.Next() {
+		// Get current node
+		node := neighboringNodes.Node()
+
+		// Create a new planning node for the expanded node
+		expandedNode := &PlanningNode{
+			Graph:            pn.Graph,
+			CurrentGraphNode: node,
+			PreviousInPlan:   pn,
+		}
+
+		// Calculate the costs for the expanded node
+		expandedNode.UpdateCosts(heuristic)
+
+		// Add the expanded node to the slice
+		expandedNodes = append(expandedNodes, expandedNode)
+	}
+
+	// Return
+	return expandedNodes
 
 }
